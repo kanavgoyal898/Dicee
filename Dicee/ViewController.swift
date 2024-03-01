807d0f7f7cb10f7b7462e2c7783798507f8b0584
Kanav Goyal <kanavgoyal898@gmail.com>
2024-03-02 01:02:14 +0530

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceArray = [
+        UIImage(imageLiteralResourceName: "DiceOne"),
+        UIImage(imageLiteralResourceName: "DiceTwo"),
+        UIImage(imageLiteralResourceName: "DiceThree"),
+        UIImage(imageLiteralResourceName: "DiceFour"),
+        UIImage(imageLiteralResourceName: "DiceFive"),
+        UIImage(imageLiteralResourceName: "DiceSix")];
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = diceArray.randomElement();
+        diceImageView2.image = diceArray.randomElement();
+    }
+    
+}
+
